456c69cf3cb217b69a3b3071d564ff3160dc7336
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-12-06 15:34:30 +0900

    change:デモ用にタスク間の依存関係を定義

--- dags/demo_dag.py
+++ dags/demo_dag.py
@@ -4,11 +4,16 @@ from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 
 
-@dag(start_date=datetime.datetime(2023, 12, 5), schedule="*/1 * * * *")
+@dag(dag_id="demo", start_date=datetime.datetime(2023, 12, 5), schedule="*/1 * * * *")
 def generate_dag():
     # ランダムに失敗するコマンドを実行
-    BashOperator(task_id="task",
-                 bash_command="exit $((RANDOM % 2))")
+    bash = BashOperator(task_id="task",
+                        bash_command="exit $((RANDOM % 2))")
+    # AWSのFargateタスクを実行する場合にはBashOperatorの代わりにEcsRunTaskOperatorを使います。
+    dependent_bash = BashOperator(task_id="dependent",
+                                  bash_command="echo after")
+
+    bash >> dependent_bash
 
 
 generate_dag()
